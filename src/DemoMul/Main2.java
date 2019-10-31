385a29fde52f469e15c9b69e56b71615ecbe23ad
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2019-10-30 22:55:56 -0700

    run and done mul

---
+++ src/DemoMul/Main2.java
@@ -0,0 +1,8 @@
+package DemoMul;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Thread t1=new Thread();
+        System.out.println(t1.getPriority());
+    }
+}
---
+++ src/DemoMul/Main3.java
@@ -0,0 +1,10 @@
+package DemoMul;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Thread t1=new Thread();
+        t1.setPriority(10);
+        System.out.println(t1);
+
+    }
+}
